decd41ec2999ab2b6537fed00c64c31fa97a11c0
Niels Breuker <Niels Breuker>
2016-12-15 11:14:53 +0100

    Openingstijden toegevoegd aan contact pagina

--- index.php
+++ index.php
@@ -38,6 +38,7 @@ using &lt;?php include(&quot;afilename.inc&quot;); ?&gt;<font SIZE="1" COLOR="#0
   <?php show_source("header.inc"); ?>
 </div>
 <p align="center"><br>
+<contact></contact>
 </p>
 <p align="center"><strong>footer.inc</strong></p>
 <div align="left" style="border:solid 2px #0000FF; background:#EAEDFC; color:#000000; padding:2px; width:800px; height:300px; overflow:auto;">
